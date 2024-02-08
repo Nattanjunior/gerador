0c749265f02482a5fafd2bb4ea5af9c1bebf5774
Nataniel Junior <fnatanieljunior@gmail.com>
2024-02-08 18:30:58 -0300

    instalamos um Slider, definimos seu estilo, adicionamos um botão de gerar senha e o estlizamos também.

--- geradorApp/App.js
+++ geradorApp/App.js
@@ -1,4 +1,5 @@
-import {View,Text, StyleSheet, Image} from 'react-native'
+import {View,Text, StyleSheet, Image, TouchableOpacity} from 'react-native'
+import Slider from '@react-native-community/slider'
 export default function App(){
   
   return(
@@ -8,6 +9,22 @@ export default function App(){
       style={styles.logo}
     />
     <Text style={styles.title}>20 caracteres</Text>
+
+    <View style={styles.areaTheSlider}> 
+      <Slider
+      style={{height: 50}}
+      minimumValue={6}
+      maximumValue={20}
+      maximumTrackTintColor='#ff0000'
+      minimumTrackTintColor='#000'
+      thumbTintColor='#392de9'
+      />
+    </View>
+
+    <TouchableOpacity style={styles.button}>
+      <Text style={styles.buttonText}>Gerar senha</Text>
+    </TouchableOpacity>
+
     </View>
   )
 }
@@ -21,6 +38,31 @@ container:{
 },
 logo:{
   marginBottom:60
+},
+areaTheSlider:{
+  marginTop: 14,
+  marginBottom: 14,
+  width: "80%",
+  backgroundColor: "#FFF",
+  borderRadius: 8,
+  padding: 8
+},
+button:{
+  backgroundColor: "#392de9",
+  width: '80%',
+  height: 50,
+  justifyContent: 'center',
+  alignItems: 'center',
+  borderRadius: 8,    
+  marginBottom: 18
+}, 
+buttonText:{
+  color: '#FFF',
+  fontSize: 20
+}, 
+title:{
+  fontSize: 30,
+  fontWeight: 'bold'
 }
 })
 
@@ -56,3 +98,13 @@ logo:{
 // <Image> tag de imagens
 
 // Vamos instalar um Slider para o projeto, <npx expo install @react-native-community/slider>
+
+// instalamos nosso slider, dentro de uma View, adicionamos o slider a ela.
+
+// para alterar a cor do Slider quando estiver no máximo, vamos usar uma propriedade chamada: maximumTrackTintColor, fornecida pelo próprio slider.
+
+// para alterar a cor do Slider quando estiver no mínimo, usar a propriedade: minumumTrackTintColor.  
+
+// para alterar a cor da bolinha do Slider, usamos a prorpiedade thumbTintColor, fornecida também pelo Slider.
+
+// importamos o TouchableOpacity, que é um  botão clicavel que tem opacidade no seu estilo  
